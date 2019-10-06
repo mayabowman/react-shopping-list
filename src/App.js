3eb311eef222d60e9a5d715d52bf006ce92e20c9
Maya Bowman <mayashady1@gmail.com>
2019-10-06 14:39:47 -0400

    worked through solution

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 import React from 'react'
 
-class App extends React.Component {
+class App extends React.Component{
   constructor() {
     super()
     this.state = {
       userInput: '',
       list: []
     }
-    this.changeUserInput = this.changeUserInput.bind(this)
   }
 
   changeUserInput(input) {
@@ -18,28 +17,113 @@ class App extends React.Component {
 
   addToList(input) {
     let listArray = this.state.list
-    listArray.push(input)
+    this.state.list.push(input)
     this.setState({
-      list: listArray,
-      userInput: ''
+      list: listArray
     })
   }
 
   render() {
     return (
-      <div className="App">
-          <input
-            type='text'
-            value={this.state.userInput}
-            onChange={ (e) => this.changeUserInput(e.target.value)}
-          />
-          <button type='submit' onClick={ () => this.addToList(this.state.userInput)}>Add</button>
+      <div>
+        <h1>Shopping List</h1>
+        <input
+          type='text'
+          value={this.state.userInput}
+          onChange={(e) => this.changeUserInput(e.target.value)}
+        ></input>
+        <button onClick={() => this.addToList(this.state.userInput)}>Add</button>
         <ul>
-          {this.state.list.map(val => <li>{val}</li>)}
+          {this.state.list.map(item => <li>{item}</li>)}
         </ul>
       </div>
     )
   }
 }
 
-export default App;
+export default App
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import React from 'react'
+
+// class App extends React.Component {
+//   constructor() {
+//     super()
+//     this.state = {
+//       userInput: '',
+//       list: []
+//     }
+//     this.changeUserInput = this.changeUserInput.bind(this)
+//   }
+
+//   changeUserInput(input) {
+//     this.setState({
+//       userInput: input
+//     })
+//   }
+
+//   addToList(input) {
+//     let listArray = this.state.list
+//     listArray.push(input)
+//     this.setState({
+//       list: listArray,
+//       userInput: ''
+//     })
+//   }
+
+//   render() {
+//     return (
+//       <div className="App">
+//         <h1>Shopping List</h1>
+//         <div>
+//             <input
+//               type='text'
+//               value={this.state.userInput}
+//               onChange={ (e) => this.changeUserInput(e.target.value)}
+//             />
+//             <button type='submit' onClick={ () => this.addToList(this.state.userInput)}>Add</button>
+//           </div>
+//         <ul>
+//           {this.state.list.map(val => <li>{val}</li>)}
+//         </ul>
+//       </div>
+//     )
+//   }
+// }
+
+// export default App;
